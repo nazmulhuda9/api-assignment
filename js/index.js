31edad3a70c3bef3baccff15a1d74c90220d325d
nazmulhuda9 <nazmulhuda7219@gmail.com>
2021-09-03 00:02:45 +0600

    div added

--- js/index.js
+++ js/index.js
@@ -1,30 +1,58 @@
+const searchResultNumber = document.getElementById("search-number")
+const searchField = document.getElementById('search-field')
+const errorDiv = document.getElementById('error');
+const divContainer = document.getElementById('div-container')
+const noResult = document.getElementById('no-result')
+
+// function number one 
 const searchButton = () => {
-    const searchField = document.getElementById('search-field')
     const searchBox = searchField.value;
-    // console.log(searchBox)
+    if (searchBox === '') {
+        errorDiv.innerText = 'search field cannot be empty';
+    } else {
+        errorDiv.innerText = ''
+    }
+
+    // clear search value 
+    searchField.value = ''
     const url = `https://openlibrary.org/search.json?q=${searchBox}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayData(data.docs))
+        // .then(data => console.log(data))
+        .then(data => displayData(data))
 }
 
-const displayData = (books) => {
-    const divContainer = document.getElementById('div-container')
+// // function two 
+const displayData = (bookses) => {
+    const resultNumber = bookses.numFound;
+    searchResultNumber.innerText = ` ${resultNumber} Result Found`;
+    if (resultNumber === 0) {
+        searchResultNumber.innerText = '';
+        noResult.innerText = `No Result Found`;
+    } else {
+        noResult.innerText = ''
+    }
+
+    const books = bookses.docs;
+    // clear container 
+    divContainer.innerHTML = ''
+
     books.forEach(book => {
-        console.log(book)
+        // console.log(book)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
     <div class="card">
-        <div class="card-body border rounded bg-primary">
-           <h4 class="card-title">Book Name: ${book.title}</h4>
-           <h5 class="card-title">Author Name: ${book.author_name[0]}</h5>
-           <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
-           <p class="card-text">Publisher: ${book.publisher[0]}</p>
+    <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg
+" class="card-img-top" alt="...">
+        <div class="card-body border rounded">
+           <h4 class="card-title">Book Name: ${book?.title}</h4>
+           <h5 class="card-title">Author Name: ${book?.author_name[0]}</h5>
+           <p class="card-text">First Publish Year: ${book?.first_publish_year}</p>
+           <p class="card-text">Publisher: ${book?.publisher[0]}</p>
        </div>
    </div>
-       `
+       `;
         divContainer.appendChild(div)
-
     });
 }
